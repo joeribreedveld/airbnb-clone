626ae0e0387e639d33a6a71c1cfbcb41010a105f
Joeri Breedveld <joeri.breedveld123@gmail.com>
2023-05-28 20:09:05 +0200

    feat(navbar): setup navbar component

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="fixed z-10 w-full bg-white shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container></Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
