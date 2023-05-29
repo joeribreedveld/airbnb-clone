ba962ba129e95c2a8e6d58c3325b1c47ad7384c6
Joeri Breedveld <joeri.breedveld123@gmail.com>
2023-05-29 10:34:57 +0200

    feat(registermodal): setup toaster error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "react-hot-toast";
 import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Toaster />
         <RegisterModal />
         <Navbar />
         {children}
