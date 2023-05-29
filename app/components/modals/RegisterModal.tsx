bf2efff43af67290ecbeade2c3ad1cb142598eb0
Joeri Breedveld <joeri.breedveld123@gmail.com>
2023-05-29 10:16:49 +0200

    feat(heading): setup heading component for in registermodal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -7,6 +7,7 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
+import Heading from "../Heading";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -42,7 +43,7 @@ const RegisterModal = () => {
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading />
+      <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
     </div>
   );
 
